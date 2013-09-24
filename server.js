ba242f75e4db10547ae15d827fdb8e9c5d124a21
Colden Cullen <coldencullen@gmail.com>
2013-09-24 10:43:38 -0400

    Added site content.

---
+++ server.js
@@ -0,0 +1,218 @@
+#!/bin/env node
+//  OpenShift sample Node application
+var express = require( 'express' ),
+    fs      = require( 'fs' ),
+    jade    = require( 'jade' ),
+    stylus  = require( 'stylus' ),
+    nib     = require( 'nib' );
+
+
+/**
+ *  Define the sample application.
+ */
+var SampleApp = function()
+{
+    //  Scope.
+    var self = this;
+    var dev  = true;
+
+    /*  ================================================================  */
+    /*  Helper functions.                                                 */
+    /*  ================================================================  */
+
+    /**
+     *  Set up server IP address and port # using env variables/defaults.
+     */
+    self.setupVariables = function()
+    {
+        //  Set the environment variables we need.
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP || process.env.IP;
+        self.port      = ( process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT ) || 8080;
+
+        if( typeof self.ipaddress === "undefined" )
+        {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            self.ipaddress = "127.0.0.1";
+        }
+    };
+
+    /**
+     *  Populate the cache.
+     */
+    self.populateCache = function()
+    {
+        if( typeof self.zcache === "undefined" )
+        {
+            self.zcache = { 'index.html': '' };
+        }
+
+        //  Local cache for static content.
+        //self.zcache[ 'index.html' ] = fs.readFileSync('./index.html');
+    };
+
+    /**
+     *  Retrieve entry (content) from cache.
+     *  @param {string} key  Key identifying content to retrieve from cache.
+     */
+    self.cache_get = function( key ) { return self.zcache[ key ]; };
+    
+    /**
+     *  Add entry to cache.
+     *  @param {string} key  Key indentifying content to add to cache.
+     *  @param {string} val  Value to add to cache.
+     */
+    self.cache_set = function( key, val ) { self.zcache[ key ] = val; };
+
+    /**
+     *  terminator === the termination handler
+     *  Terminate server on receipt of the specified signal.
+     *  @param {string} sig  Signal to terminate on.
+     */
+    self.terminator = function( sig )
+    {
+        if( typeof sig === "string" )
+        {
+           console.log( '%s: Received %s - terminating sample app ...',
+                       Date( Date.now() ), sig );
+           process.exit( 1 );
+        }
+        console.log( '%s: Node server stopped.', Date( Date.now() ) );
+    };
+
+    /**
+     *  Setup termination handlers (for exit and a list of signals).
+     */
+    self.setupTerminationHandlers = function()
+    {
+        //  Process on exit and signals.
+        process.on( 'exit', function() { self.terminator(); } );
+
+        // Removed 'SIGPIPE' from the list - bugz 852598.
+        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+        ].forEach( function( element, index, array )
+        {
+            process.on( element, function() { self.terminator(element); } );
+        } );
+    };
+
+    /*  ================================================================  */
+    /*  App server functions (main app logic here).                       */
+    /*  ================================================================  */
+
+    self.buildPage = function( req, res )
+    {
+        // Set header for response
+        res.setHeader('Content-Type', 'text/html');
+        
+        // Try to find page in cache.
+        var cachedPage = self.cache_get( req.route.path );
+        
+        if( dev || typeof cachedPage === "undefined" )
+        {
+            self.app.render(
+                'index',
+                { routePath: req.route.path },
+                function( err, html )
+                {
+                    self.cache_set( req.route.path, html );
+                    res.send( html );
+                }
+            );
+        }
+        else
+        {
+            return res.send( cachedPage );
+        }
+    };
+
+    /**
+     *  Create the routing table entries + handlers for the application.
+     */
+    self.createRoutes = function()
+    {
+        self.routes = { };
+
+        self.routes[ '/' ] =
+        self.routes[ '/about' ] =
+        self.routes[ '/resume' ] =
+        self.routes[ '/projects' ] =
+        self.routes[ '/blog' ] =
+            self.buildPage;
+    };
+
+    /**
+     *  Initialize the server (express) and create the routes and register
+     *  the handlers.
+     */
+    self.initializeServer = function()
+    {
+        self.createRoutes();
+        self.app = express();
+
+        //  Add handlers for the app (from the routes).
+        for( var r in self.routes )
+        {
+            self.app.get( r, self.routes[ r ] );
+        }
+        
+        self.app.set( 'views', __dirname + '/views' );
+        self.app.set( 'view engine', 'jade' );
+        
+        //self.app.use( express.logger( 'dev' ) )
+        
+        self.app.use(
+            stylus.middleware(
+                {
+                    src:    __dirname + '/',
+                    dest:   __dirname + '/static',
+                    force:  dev,
+                    compile:function( str, path )
+                    {
+                        return stylus( str )
+                            .set( 'filename', path )
+                            .use( nib() )
+                            .import( 'nib' );
+                    }
+                }
+            )
+        );
+        
+        self.app.use( express.static( __dirname + '/static' ) );
+    };
+
+    /**
+     *  Initializes the sample application.
+     */
+    self.initialize = function()
+    {
+        self.setupVariables();
+        self.populateCache();
+        self.setupTerminationHandlers();
+
+        // Create the express server and routes.
+        self.initializeServer();
+    };
+
+    /**
+     *  Start the server (starts up the sample application).
+     */
+    self.start = function()
+    {
+        //  Start the app on the specific interface (and port).
+        self.app.listen(self.port, self.ipaddress, function() {
+            console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), self.ipaddress, self.port);
+        } );
+    };
+};   /*  Sample Application.  */
+
+/**
+ *  main():  Main code.
+ */
+var zapp = new SampleApp();
+zapp.initialize();
+zapp.start();
+
---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+$( function($) {
+    $(document).foundation();
+    
+    $( "#routePath" ).html( routePath );
+} );
