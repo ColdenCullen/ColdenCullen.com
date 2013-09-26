d0bd6561650ffdf27687b65eedf8426d190103db
Colden Cullen <coldencullen@gmail.com>
2013-09-26 16:43:05 -0400

    Updated pagr.

--- static/js/script.js
+++ static/js/script.js
@@ -2,13 +2,4 @@ $( function($) {
     $(document).foundation();
     
     pagescroller( { initialPage: routePath } );
-    
-    // Setup link click events
-    // $( '.pagelink' ).click( function( event ) {
-    //     event.preventDefault();
-    //     scrollToPage( $( event.target ).attr( 'href' ) );
-    // } );
-    
-    // Scroll to inital page
-    //scrollToPage( routePath );
 } );
