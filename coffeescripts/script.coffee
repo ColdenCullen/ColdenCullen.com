0c6d05d773c3cc191f1a8ce3c4cff5c57b25715f
Colden Cullen <coldencullen@gmail.com>
2013-10-09 16:06:25 -0400

    Removed Semantic stuff.

--- coffeescripts/script.coffee
+++ coffeescripts/script.coffee
@@ -1,19 +1,5 @@
-initSemantic = () ->
-    # Setup dropdowns
-    $( '.ui.dropdown' ).dropdown { on: 'hover', delay: { show: 0, hide: 0 } }
-    
-    $topbar = $( '.ui.menu.fixed' )
-    
-    # Setup top bar
-    $( 'body' ).css 'padding-top', do $topbar.height + 'px'
-    $topbar.css 'margin-top', '-' + do $topbar.height + 'px'
-
 jQuery ($) =>
     
-    do $( document ).foundation unless semantic
+    do $( document ).foundation
     
-    #console.log "Testing Coffee!"
-    
-    pagr { initialPage: routePath }
-    
-    do initSemantic if semantic
+    do pagr { initialPage: routePath }
--- server.js
+++ server.js
@@ -117,7 +117,7 @@ var Site = function()
             // Render the page
             self.app.render(
                 'index',
-                { routePath: req.route.path, dev: dev, semantic: false },
+                { routePath: req.route.path, dev: dev },
                 function( err, html ) {
                     self.cache_set( req.route.path, html );
                     res.send( html );
@@ -180,8 +180,8 @@ var Site = function()
         self.app.set( 'views', __dirname + '/views' );
         self.app.set( 'view engine', 'jade' );
         
-        //if( debug )
-        //    self.app.use( express.logger( 'dev' ) );
+        if( debug )
+            self.app.use( express.logger( 'dev' ) );
         
         //Setup stylus middleware
         self.app.use( stylus.middleware( {
